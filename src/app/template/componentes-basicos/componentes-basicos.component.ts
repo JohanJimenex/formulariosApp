63b673241d4f03f54968bda50283490680f49aba
Johan Jiménez <johan.jimenez.23@gmail.com>
2022-08-20 16:33:08 -0400

    form template caja bananas

--- src/app/template/componentes-basicos/componentes-basicos.component.ts
+++ src/app/template/componentes-basicos/componentes-basicos.component.ts
@@ -9,25 +9,29 @@ export class ComponentesBasicosComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   //Viene un objeto tipo json con todos los campos del formulario
   @ViewChild('miForm') miForm!: NgForm;
 
   // guardar(miForm: NgForm) {
   guardar() {
-    // console.log(this.miForm.value);
+
+    this.miForm?.resetForm({
+      pre: 0,
+      exi: 0 
+    });
+
   }
 
   validarNombreProd(): boolean {
     return this.miForm?.controls['pro']?.invalid
-      && this.miForm?.controls['pro'].touched;
+      && this.miForm?.controls['pro']?.touched;
   }
 
   validarPrecio(): boolean {
     // console.log(this.miForm?.controls['pre']?.value);
-   
+
     if (this.miForm?.controls['pre']?.value <= 0
       && this.miForm?.controls['pre']?.touched) {
       return true;
--- src/app/template/componentes-dinamicos/componentes-dinamicos.component.ts
+++ src/app/template/componentes-dinamicos/componentes-dinamicos.component.ts
@@ -1,14 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Persona {
+  nombre: string,
+  favoritos: Favoritos[]
+}
+
+interface Favoritos {
+  id: number,
+  nombre: string
+
+}
+
 @Component({
   selector: 'app-componentes-dinamicos',
-  templateUrl: './componentes-dinamicos.component.html'
+  templateUrl: './componentes-dinamicos.component.html',
+
 })
-export class ComponentesDinamicosComponent implements OnInit {
+export class ComponentesDinamicosComponent {
 
-  constructor() { }
+  personas: Persona[] = [];
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Johan',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre: 'Super Mario 64'
+      }
+    ]
   }
 
+  favorito: Favoritos = {
+    id: this.persona.favoritos.length + 1,
+    nombre: ''
+  }
+
+  guardar(): void {
+    console.log('klk');
+  }
+
+  eliminar(item: Favoritos): void {
+    let i = this.persona.favoritos.indexOf(item);
+
+    this.persona.favoritos.splice(i, 1);
+
+  }
+
+  agregar(): void {
+    this.persona.favoritos.push(this.favorito);
+
+    this.favorito = { id: 0, nombre: '' }
+  }
 }

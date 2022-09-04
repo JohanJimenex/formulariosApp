1d89f367c0962969b9a191be8cf0967346e21414
Johan Jiménez <johan.jimenez.23@gmail.com>
2022-09-04 00:50:12 -0400

    validaciones personalizadas

--- src/app/validaciones/pages/registro/registro.component.ts
+++ src/app/validaciones/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -13,6 +13,18 @@ export class RegistroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  noPuedeSerJohan(control: FormControl) {
+
+    if (control.value.trim().toLowerCase() == 'johan') {
+      return {
+        error: true
+      }
+    }
+    return null;
+
+  }
+
   //expresion regular, acepta nombre de la A a la Z mas cualqueir valor 
   nombreYApellidoRegEx: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailRegEx: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
@@ -21,6 +33,7 @@ export class RegistroComponent implements OnInit {
     {
       nombre: ['', [Validators.required, Validators.pattern(this.nombreYApellidoRegEx)]],
       email: ['', [Validators.required, Validators.email, Validators.pattern(this.emailRegEx)]],
+      usuario: ['', [Validators.required, this.noPuedeSerJohan]],
     }
   );
 
